8a7dd23b18d40b32bbeb124db8c8d195db171312
Kelvin <kckelvin73@gmail.com>
2025-12-25 23:49:10 +0100

    added the score variabes and one round play of the game

--- Script.js
+++ Script.js
@@ -1,16 +1,41 @@
+let humanScore = 0;
+let computerScore = 0;
 
 
-
-let getHumanchoice = prompt("Please enter your selection: Rock Paper or Scissors?");
-
-console.log(getHumanchoice);
+function getHumanChoice() {
+    const humanChoice =
+    prompt("Please enter your choice Rock, Paper or Scissors").toLowerCase();
+    return humanChoice;
+}
+console.log(getHumanChoice());
 
 function getComputerChoice() {
-     
-    const choices = ["rock", "paper", "scissors"]
+    const computerChoice = Math.floor(Math.random() * 3);
 
-    const randomIndex = Math.floor(Math.random() * 3);
-
-    return choices[randomIndex];
+    if (computerChoice === 0) {
+        return "rock";
+    } else if (computerChoice === 1){
+        return "paper";
+    } else return "scissors";
 }
 console.log(getComputerChoice());
+
+function playRound() {
+   const humanScoren = humanChoice();
+    computerScore = computerChoice();
+    
+    if (humanChoice === computerChoice) {
+        console.log("Its a tie! Both chose ${humanChoice}");
+        return;
+    }
+    else if (humanChoice === "rock" &&computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        console.log('You win! ${humanScore} beat ${computerScore}')
+    }
+    else {
+        computerScore++;
+        console.log("You lose! ${computerScore} beat ${humanScore}")
+    }
+
+}
+console.log(playRound());
